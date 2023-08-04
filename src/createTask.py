ef6cd3126adddff23d460583dc06f8e35ce46f12
smitagg <54025112+smitagg@users.noreply.github.com>
2023-08-04 10:57:07 +0530

    Update createTask.py

--- src/createTask.py
+++ src/createTask.py
@@ -1,4 +1,14 @@
 import sys
+import argparse
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
+
+parser = parser.argparse.ArgumentParser()
+
+parser.add_argument('--myArg1')
+parser.add_argument('--myArg2')
+known_args,new_args = parser.parse_known_args()
+
+print(known_args)
+print(new_args)
