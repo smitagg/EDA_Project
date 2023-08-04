e1a177841b21db7a060d4572a34df7551bed68d8
smitagg <54025112+smitagg@users.noreply.github.com>
2023-08-04 11:07:56 +0530

    Update createTask.py

--- src/createTask.py
+++ src/createTask.py
@@ -11,9 +11,9 @@ parser.add_argument('--myArg1',dest="a1")
 parser.add_argument('--myArg2', dest="a2")
 known_args,new_args = parser.parse_known_args()
 
-print(known_args)
-print(new_args)
+a1 = known_args.a1.replace("'",'"')
+a2 = known_args.a2.replace("'",'"')
 
 
-print(json.loads(known_args.a1))
-print(json.loads(known_args.a2))
+print(json.loads(a1))
+print(json.loads(a2))
